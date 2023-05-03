f84accb002e3f132dc2db47abb67103495ee6f4a
AAstahckin <ALVAstashkin91@yandex.ru>
2023-05-03 21:27:19 +0300

    fis

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,5 +1,4 @@
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exist;
@@ -8,11 +7,11 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.By.linkText;
 
-public class SelenideTest {
+public class SelenideTest extends TestBase {
 
     @Test
+    @Step("Тест1")
     public void testIssueSearch() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
 
         open("https://github.com");
 
